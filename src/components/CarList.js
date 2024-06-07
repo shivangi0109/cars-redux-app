cca05ad094b330a7e3f2b7d91097e41a178818f1
Shivangi Srivastava <shi06dec@gmail.com>
2024-06-07 14:32:24 -0600

    List the Cars records

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -5,9 +5,32 @@ function CarList() {
     return state.cars.data;
   });
 
-  console.log(cars);
+  const handleCarDelete = (car) => {
+    // ...
+  };
 
-  return <div>CarList</div>;
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
