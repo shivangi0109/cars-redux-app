4a525bdc1c6c2fb7c5fda25e82e11b84831ddcd6
Shivangi Srivastava <shi06dec@gmail.com>
2024-06-07 14:27:33 -0600

    Awkward Double Keys for cars removed

--- src/store/slices/carsSlice.js
+++ src/store/slices/carsSlice.js
@@ -4,7 +4,7 @@ const carsSlice = createSlice({
   name: 'cars',
   initialState: {
     searchTerm: '',
-    cars: []
+    data: []
   },
   reducers: {
     changesearchTerm(state, action) {
@@ -13,7 +13,7 @@ const carsSlice = createSlice({
     addCar(state, action) {
       // Assumption
       // action.payload === { name: 'a', cost: 120 }
-      state.cars.push({
+      state.data.push({
         name: action.payload.name,
         cost: action.payload.cost,
         id: nanoid()
@@ -22,10 +22,10 @@ const carsSlice = createSlice({
     removeCar(state, action) {
       // Assumption
       // action.payload === id of the car we want to remove
-      const updated = state.cars.filter((car) => {
+      const updated = state.data.filter((car) => {
         return car.id !== action.payload;
       });
-      state.cars = updated;
+      state.data = updated;
     },
   },
 });
