10e273fa27594ad5d45dbf76f2d37335311821dc
Shivangi Srivastava <shi06dec@gmail.com>
2024-06-07 13:41:33 -0600

    Create store and in CarForm component added form name values to update state

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,5 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeName } from "../store";
+
 function CarForm() {
-  return <div>CarForm</div>;
+  const dispatch = useDispatch();
+
+  const handleNameChange = (event) => {
+    dispatch(changeName(event.target.value));
+  };
+
+  const name = useSelector((state) => {
+    return state.form.name;
+  });
+
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CarForm;
