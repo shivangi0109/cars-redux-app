27a6b3594f2930fb1854c3db4cbe7cdf5c8dfc9a
Shivangi Srivastava <shi06dec@gmail.com>
2024-06-11 16:04:48 -0600

    Hightlighting Existing Cars

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,13 +1,17 @@
-import { useSelector, useDispatch } from "react-redux";
-import { removeCar } from "../store";
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCar } from '../store';
 
 function CarList() {
   const dispatch = useDispatch();
-
-  const cars = useSelector(({ cars: { data, searchTerm }}) => {
-    return data.filter((car) =>
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
       car.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
   });
 
   const handleCarDelete = (car) => {
@@ -15,8 +19,11 @@ function CarList() {
   };
 
   const renderedCars = cars.map((car) => {
+    // DECIDE IF THIS CAR SHOULD BE BOLD
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
